4ea2be5586104a3945222f51fb91df0559029746
craigmcginley <craigmcginley@gmail.com>
2019-10-28 13:07:33 -0400

    add HALE data and state

--- src/world-map.js
+++ src/world-map.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { extent } from 'd3-array'
 import { geoMercator, geoPath } from 'd3-geo'
-import { scaleOrdinal } from 'd3-scale'
+import { scaleLinear, scaleOrdinal } from 'd3-scale'
 import { schemeCategory10 } from 'd3-scale-chromatic'
 import { feature } from 'topojson-client'
 
-import countries from 'world-atlas/countries-10m.json'
-import countryTypes from './data.json'
+import countries from 'world-atlas/countries-110m.json'
+import countryData from './data.json'
 
 const systemTypes = ["Universal", "Universal-Income", "Private-Public", "Private-Subsidized", "Private-Partially-Subsidized"]
 const antarcticaId = "010"
+
+// TODO: Stopped data at Kiribati
 
 class WorldMap extends Component {
   constructor(props) {
@@ -16,8 +19,13 @@ class WorldMap extends Component {
 
     this.state = {
       width: 1200,
-      height: 900
+      height: 900,
+      hale: false
     }
+  }
+
+  setHale = (e) => {
+    this.setState({ hale: e.target.checked });
   }
 
   render() {
@@ -25,12 +33,17 @@ class WorldMap extends Component {
     const features = data.features.filter(d => d.id !== antarcticaId);
     const projection = geoMercator()
       .fitSize([this.state.width, this.state.height], data);
-    const mappedData = new Map(countryTypes.map(country => {
-        return [country.name, { type: country.type }];
+    const mappedData = new Map(countryData.map(country => {
+        return [country.name, { type: country.type, hale: country.hale }];
       }));
     const color = scaleOrdinal()
       .domain(["Universal", "Universal-Income", "Private-Public", "Private-Subsidized", "Private-Partially-Subsidized"])
       .range(schemeCategory10);
+
+    const haleData = countryData.map(d => d.hale);
+    const hale = scaleLinear()
+      .domain(extent(haleData))
+      .range([0, 1]);
 
     return (
       <div>
@@ -39,12 +52,14 @@ class WorldMap extends Component {
             {
               features.map((d, i) => {
                 const obj = mappedData.get(d.properties.name);
+                const opacity = this.state.hale ? ((obj && hale(obj.hale)) || 0) : 1;
                 return (
                   <path
                     key={ `path-${ i }` }
                     d={ geoPath().projection(projection)(d) }
                     className={`country ${d.type}`}
-                    fill={ (obj && color(obj.type)) }
+                    fill={ obj ? color(obj.type) : '#f0f0f0' }
+                    style={{ opacity: opacity }}
                     stroke="#FFFFFF"
                     strokeWidth={ 0.5 }
                   />
@@ -52,21 +67,13 @@ class WorldMap extends Component {
               })
             }
           </g>
-          {/* <g className="markers">
-            <circle
-              cx={ this.projection()([8,48])[0] }
-              cy={ this.projection()([8,48])[1] }
-              r={ 10 }
-              fill="#E91E63"
-              className="marker"
-            />
-          </g> */}
         </svg>
         <ul className="legend">
           { systemTypes.map((d, i) => {
-            return <li>{d}: <div className="legend__block" style={{ backgroundColor: color(d) }}></div></li>
+            return <li key={d}>{d}: <div className="legend__block" style={{ backgroundColor: color(d) }}></div></li>
           })}
         </ul>
+        <input type="checkbox" checked={this.state.hale} onChange={this.setHale} />
       </div>
     )
   }
